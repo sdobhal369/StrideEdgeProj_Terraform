3d3e684d99492ce10aa67745c0c92e990f8f391b
sdobhal369 <shubham.dobhal@successive.tech>
2022-06-15 16:03:48 +0530

    Updated Files

--- providers.tf
+++ providers.tf
@@ -3,7 +3,3 @@
 #       access_key =  var.access_key
 #       secret_key = var.secret_key
 # }
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
--- script.sh
+++ script.sh
@@ -1,15 +1,57 @@
 #!bin/sh
 
-helm repo add oteemocharts https://oteemo.github.io/charts
-helm repo update
 
-helm install sonarqube oteemocharts/sonarqube
-helm install sonatype-nexus oteemocharts/sonatype-nexus
+helm_status_1=$(helm status sonarqube -n sonarqube -o json | jq .status.phase -r)
+helm_status_2=$(helm status sonatype-nexus -n nexus -o json | jq .status.phase -r)
 
-kubectl apply -f jmeter_master_deploy.yaml
-kubectl apply -f jmeter_slaves_deploy.yaml
-kubectl apply -f jmeter_master_configmap.yaml
-kubectl apply -f jmeter_slaves_svc.yaml
+kubernetes_status=$(kubectl get namespace jmeter -o json | jq .status.phase -r) 
 
-kubectl expose service sonarqube-sonarqube --type=NodePort --target-port=9000 --name=sonarqube
+## Checking if Helm files are Present
 
+if [[ $helm_status_1 == "null" && $helm_status_2 == "null" ]]
+
+then
+
+    echo "Helm Files are already present. Hence removing..."
+    
+    kubectl delete namespace sonarqube
+    kubectl delete namespace nexus
+    helm repo remove oteemocharts
+    helm repo update
+
+else
+
+    echo "Creating Helm Files..."
+    
+    helm repo add oteemocharts https://oteemo.github.io/charts
+    helm repo update
+    kubectl create namespace sonarqube
+    kubectl create namespace nexus    
+    helm install sonarqube oteemocharts/sonarqube -f sonarqube-values.yaml --namespace=sonarqube
+    helm install sonatype-nexus oteemocharts/sonatype-nexus -f nexus-values.yaml --namespace=nexus
+
+fi
+
+## Checking if Kubernetes files are Present
+
+
+if [[ $kubernetes_status == "Active" ]]
+
+then
+
+    echo "Kubernetes Namespace is already present. Hence removing..."
+    
+    kubectl delete namespace jmeter
+    
+    
+else
+
+    echo "Creating Kubernetes Files..."
+    
+    kubectl create namespace jmeter
+    kubectl apply -f jmeter_master_deploy.yaml
+    kubectl apply -f jmeter_slaves_deploy.yaml
+    kubectl apply -f jmeter_master_configmap.yaml
+    kubectl apply -f jmeter_slaves_svc.yaml
+
+fi
